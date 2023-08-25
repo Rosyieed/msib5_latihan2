7308e77d51b1d89006a49b7a4e9efa97a647d071
Rosyieed <rosyid123.ren@gmail.com"git>
2023-08-25 21:08:47 +0700

    Tugas 2(done)

---
+++ script.js
@@ -0,0 +1,38 @@
+const result = document.getElementById("hasil");
+
+document.getElementById("hitungButton").addEventListener("click", function () {
+  const nama = document.getElementById("nama").value;
+  const matkul = document.getElementById("matkul").value;
+  const grade = document.getElementById("grade").value;
+
+  let nilaiSkala = 0;
+  switch (grade) {
+    case "A":
+      nilaiSkala = 4;
+      break;
+    case "B":
+      nilaiSkala = 3;
+      break;
+    case "C":
+      nilaiSkala = 2;
+      break;
+    case "D":
+      nilaiSkala = 1;
+      break;
+    case "E":
+      nilaiSkala = 0;
+      break;
+  }
+
+  const rataRata = nilaiSkala;
+
+  const newRow = `
+        <tr>
+          <td>${nama}</td>
+          <td>${matkul}</td>
+          <td>${grade}</td>
+          <td>${rataRata}</td>
+        </tr>
+      `;
+  result.innerHTML += newRow;
+});
